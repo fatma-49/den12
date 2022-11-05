b6aafec8ca22493b379948c3ebf5512bd0d72b4d
Fatma Görür <fkumgorur@gmail.com>
2022-11-05 18:13:45 +0300

    v1

--- src/main/java/fay/h.java
+++ src/main/java/fay/h.java
@@ -4,5 +4,11 @@ public class h {
 
     public static void main(String[] args) {
         System.out.println("hdhjdj");
+      /*  git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/fatma-49/den12.git
+        git push -u origin main
+
+       */
     }
 }
---
+++ src/main/java/fay/hsd.java
@@ -0,0 +1,14 @@
+package fay;
+
+public class hsd {
+
+
+    public static void main(String[] args) {
+        System.out.println("hsjdsd");
+        System.out.println("hsjdsd");
+        System.out.println("hsjdsd");
+        System.out.println("hsjdsd");
+        System.out.println("hsjdsd");
+        System.out.println("hsjdsd");
+    }
+}
